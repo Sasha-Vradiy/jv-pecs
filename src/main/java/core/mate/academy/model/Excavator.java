46c0214a1063029edb13d8412e5f1e68bc74f7c9
Oleksandr Vradii <alexvradii@gmail.com>
2025-10-23 12:59:19 +0300

    Make getAll return mutable list and add Excavator setter.

--- src/main/java/core/mate/academy/model/Excavator.java
+++ src/main/java/core/mate/academy/model/Excavator.java
@@ -18,6 +18,7 @@ public class Excavator extends Machine {
     public double getBucketCapacityCubikMeters() {
         return bucketCapacityCubikMeters;
     }
+
     public void setBucketCapacityCubicMeters(double bucketCapacityCubicMeters) {
         this.bucketCapacityCubikMeters = bucketCapacityCubicMeters;
     }
